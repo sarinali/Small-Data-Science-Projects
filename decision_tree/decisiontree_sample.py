c831c31dbfbac3fefce7a679c6b8888558ee470e
Sarina Li <74844953+sarinali@users.noreply.github.com>
2023-05-09 23:30:18 -0400

    added csv and imports

---
+++ decision_tree/decisiontree_sample.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+
+col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
+# load dataset
+pima = pd.read_csv("diabetes.csv", header=None, names=col_names)
+
